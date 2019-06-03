8cbe43bcbad9f0300484dcbcf38ee1cc64c1437f
Dotsung <andy395023@gmail.com>
2019-06-03 23:51:16 +0900

    container

---
+++ src/containers/EditorContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import oc from 'open-color';
+
+class EditorContainer extends React.Component {
+    render(){
+        return (
+            <>
+            </>
+        )
+    }
+}
