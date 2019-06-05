edffbb5e62800a1d0d0be9c67f7ec77f1275202c
Dotsung <andy395023@gmail.com>
2019-06-05 15:10:44 +0900

    object test

--- src/components/GameObject.js
+++ src/components/GameObject.js
@@ -11,20 +11,29 @@ const Box = styled.div`
 `
 
 class GameObject extends React.Component {
-    eventLogger = (e, data) => {
-        console.log('Event: ', e);
-        console.log('Data: ', data);
-    };
+
+    state = {
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100
+    }
 
     render() {
+        console.log(this.state);
         return (
             <Rnd
-                default= {{
-                    x: 0,
-                    y: 0,
-                    width: 100,
-                    height: 100
+                size={{ width: this.state.width, height: this.state.height }}
+                position={{ x: this.state.x, y: this.state.y }}
+                onDragStop={(e, d) => { this.setState({ x: d.x, y: d.y }) }}
+                onResize={(e, direction, ref, delta, position) => {
+                    this.setState({
+                      width: ref.style.width,
+                      height: ref.style.height,
+                      ...position,
+                    });
                 }}
+                bounds=".Editor"
             >
                 <Box className="handle" />
             </Rnd>
