b6b7698ecc530444de3baca22009d19b90f859fc
Dotsung <andy395023@gmail.com>
2019-06-08 17:21:54 +0900

    objStore

--- src/stores/objStore.js
+++ src/stores/objStore.js
@@ -1,5 +1,31 @@
-class objStore {
+import { observable, action, computed } from 'mobx';
 
+class objStore {
+    @observable objects = [];
+
+    @action
+    put = (obj) => {
+        this.objects.push(obj);
+        return;
+    }
+
+    @action
+    remove = (index) => {
+        this.objects = this.objects.splice(index, 1);
+        return;
+    }
+
+    @action
+    move = (index, x, y) => {
+        this.objects[index].x = x;
+        this.objects[index].y = y;
+    }
+
+    @action
+    changeSize = (index, width, height) => {
+        this.objects[index].width = width;
+        this.objects[index].height = height;
+    }
 }
 
 export default objStore;
