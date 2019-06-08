c9d8ce8d18a7e8688c9200c24e14bc80f6cc9ebe
Dotsung <andy395023@gmail.com>
2019-06-08 17:29:29 +0900

    sidebar 화면구성

--- src/containers/SideBarContainer.js
+++ src/containers/SideBarContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
+import { observer, inject } from 'mobx-react'
 
 const Container = styled.div`
     height: 100%;
@@ -8,11 +9,47 @@ const Container = styled.div`
     background-color: ${oc.red[5]};
 `
 
+const Wrapper = styled.div`
+    margin: 30px;
+    display: flex;
+    flex-direction: column;
+`
+
+const ObjList = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: ${oc.red[7]};
+    width: 100%;
+    height: 400px;
+`
+
+const ObjProperty = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: ${oc.red[7]};
+    width: 100%;
+    height: 400px;
+`
+
+const CreateButton = styled.button`
+`
+
+@inject('objStore')
+@observer
 class SideBarContainer extends React.Component {
     render(){
         return (
             <Container>
-                SideBar
+                <Wrapper>
+                    objList
+                    <ObjList>
+
+                    </ObjList>
+                    ObjProperty
+                    <ObjProperty>
+
+                    </ObjProperty>
+                </Wrapper>   
             </Container>
         )
     }
